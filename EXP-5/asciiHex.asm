9290dba55c68f1c9727f80eb4728cd45abc76697
Shefa <shefaoudeen123@gmail.com>
2024-02-02 20:44:00 +0530

    avlo than

--- EXP-5/asciiHex.asm
+++ EXP-5/asciiHex.asm
@@ -1,62 +1,40 @@
-data segment
-  myStr db 'S','H','E','F','A'
-  res db 10 dup ('$')
-  looping dw 5
-  index dw 0h
-ends
+org 100h
+.data
+mystr DB 'A','B','C'
+RES  DB 10 DUP ('$')  
+.code
 
-code segment
-start:
-  mov ax,data
-  mov ds,ax
-  mov es,ax
-  mov cx, [looping]
-
-  conversion:
-  xchg cx, [looping]
-  mov si, index
-  lea si,myStr[si] 
-  mov ax, [si]
-  mov ah,0
-  lea si,res
-  call ascii2hex
-  lea dx, res
-  mov ah,9
-  int 21h
-  call comma
-  inc index
-  mov cx,[looping]
-  loop conversion
-  jmp final
-
-ascii2hex proc
-  mov cx,0
-  mov bx,10
-  loop1:
-    mov dx,0
-    div bx
-    add dl,30h
-    push dx
-    inc cx
-    cmp ax,9H
-    jg loop1
-    add al,30h
-    mov [si], al
-  loop2:
-    pop ax
-    inc si
-    mov [si],al
-    loop loop2
-  ret
-ascii2hex endp  
-
-comma proc 
-mov dl, ' '
+mov cx, 3
+lea si,mystr
+loop conversion:
+mov AX,[si]                               
+mov ah,0
+    LEA SI,RES
+    CALL HEX2DEC
+    LEA DX,RES
+    MOV AH,9
+    INT 21H
+mov dl,' '
 mov ah,2
-int 21h
-ret
-comma endp
+int 21h 
+loop conversion
 
-final:
-ends
-end start
+HEX2DEC PROC NEAR
+    MOV CX,0
+    MOV BX,10
+LOOP1: MOV DX,0
+       DIV BX
+       ADD DL,30H
+       PUSH DX
+       INC CX
+       CMP AX,9
+       JG LOOP1
+       ADD AL,30H
+       MOV [SI],AL
+LOOP2: POP AX
+       INC SI
+       MOV [SI],AL
+       LOOP LOOP2
+       RET
+HEX2DEC ENDP           
+END START 
--- EXP-5/hex2ascii.asm
+++ EXP-5/hex2ascii.asm
@@ -1,20 +1,36 @@
 data segment
-  hex db 9Ah
-  ascii dw ?
+    hexa db 40h
+    ascii dw ?
 ends
 
 code segment
-  start:
-    mov ax,data
-    mov ds,ax
-    mov es,ax
-    
-    mov ah,00h
-    mov ah,al
-    and ah, 0f0h
-    MOV cl,04h
-    ror ah,cl
-    add ah,30h
-    cmp ah,39h
-    jle s
-    
+    start:
+       mov ax,data
+       mov ds,ax
+       mov es,ax
+       
+       mov ah,hexa
+       and ah, 0f0h
+       mov cl,04h
+       ror ah,cl
+       add ah,30h
+       cmp ah,39h
+       jle s
+       and ah,07h
+       
+    s:
+        mov bh,ah
+        and al,0fh
+        add al,30h
+        cmp al,39h
+        jle s1
+        add al,07h
+        
+    s1:
+        mov cl,al
+        add bx,cx
+        mov ascii,bx
+        mov ax, 4c00h
+        int 21h
+ends
+end start
